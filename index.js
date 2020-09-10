869848c43ce4f8f5874353cd19403b936e214d81
HmLee1024 <rhrlfkd1111@naver.com>
2020-09-10 16:05:53 +0900

    정보수정

--- index.js
+++ index.js
@@ -2,16 +2,40 @@
 const express = require('express') 
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
 const mongoode = require('mongoose')
+const config = require('./config/key')
 
-mongoode.connect("mongodb+srv://hyemin:multi1004@bolier-plate.f2gxy.mongodb.net/boiler-plate?retryWrites=true&w=majority", {
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+//DB 연결
+mongoode.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
 .then (() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요 끼루룩')
+})
+
+//화원가입의 필요한 정보를 client에서 가져와 DB에 저장
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    //mongoDB 메소드 save
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+
 })
 
 app.listen(port, () => {
